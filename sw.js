d9373421c87cb061493e29ad5afba1b4b7035722
noelsantosjr6-cloud <noelsantosjr6@gmail.com>
2026-06-04 12:20:00 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+(function () {
+'use strict';
+var CACHE_NAME = 'stock-locator-cache-v1';
+var urlsToCache = [
+'stock_locator_enhanced.html',
+'manifest.json'
+];
+
+self.addEventListener('install', function (event) {
+event.waitUntil(
+caches.open(CACHE_NAME).then(function (cache) {
+return cache.addAll(urlsToCache);
+})
+);
+});
+
+self.addEventListener('activate', function (event) {
+event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function (event) {
+var request = event.request;
+event.respondWith(
+caches.match(request).then(function (response) {
+return response || fetch(request);
+})
+);
+});
+})();
