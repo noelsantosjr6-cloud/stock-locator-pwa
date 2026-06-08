494b2f09a7d8ec29c5d39c2e1014b2f7b58c32f9
noelsantosjr6-cloud <noelsantosjr6@gmail.com>
2026-06-08 08:59:48 +0800

    Update service worker cache version to v9

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'stock-locator-v8'; // Changing this version forces a cache reset
+const CACHE_NAME = 'stock-locator-v9'; // Changing this version forces a cache reset
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
